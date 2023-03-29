8e54f0c752c7ac7f91e2fec3c8ca22e19b72e7ae
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-03-29 13:03:36 +0300

    Initial Commit

---
+++ app/src/main/java/com/emmanuel_rono/potterpedia/ListDetails.kt
@@ -0,0 +1,14 @@
+package com.emmanuel_rono.potterpedia
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ListDetails : Fragment(R.layout.fragment_list_details)
+
+{
+
+}
---
+++ app/src/main/java/com/emmanuel_rono/potterpedia/harryList.kt
@@ -0,0 +1,13 @@
+package com.emmanuel_rono.potterpedia
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class harryList : Fragment(R.layout.fragment_harry_list) {
+
+
+}
