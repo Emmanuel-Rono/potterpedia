c2ccf9335bf3a60542284d29987be655926e6a45
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-03-30 14:22:09 +0300

    progress

---
+++ app/src/main/java/com/emmanuel_rono/potterpedia/HarryAdapter.kt
@@ -0,0 +1,26 @@
+package com.emmanuel_rono.potterpedia
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.emmanuel_rono.potterpedia.databinding.DatalayoutBinding
+
+
+class HarryAdapter:RecyclerView.Adapter<HarryAdapter.myViewHolder>()
+{
+    inner class myViewHolder(binding: DatalayoutBinding):RecyclerView.ViewHolder(binding.root)
+    {
+
+
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): myViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: myViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
